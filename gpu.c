897d94ba8af662ae3c62a8a871a31a35d69752c2
Ryohei Tokuda <tokuda@idein.jp>
2017-12-13 20:11:37 +0900

    fix

--- gpu.c
+++ gpu.c
@@ -12,7 +12,7 @@
 #define MAX_SOURCE_SIZE (0x100000)
 
 int main(int argc, char * argv[]) {
-  if (! argc == 5) {
+  if (! (argc == 5)) {
     printf ("usage: %s filename name arg_num data_num\n", argv[0]);
     exit(0);
   }
@@ -20,7 +20,7 @@ int main(int argc, char * argv[]) {
   char * kernel_name = argv[2];
   int arg_num = atoi(argv[3]);
   int N = atoi (argv[4]);
-    
+
   cl_device_id device_id = NULL;
   cl_context context = NULL;
   cl_command_queue command_queue = NULL;
@@ -52,9 +52,7 @@ int main(int argc, char * argv[]) {
   ret = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
 
   context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-  printf ("context %d\n", ret);
   command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-  printf ("command queue %d\n", ret);
 
   program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
   assert (ret == CL_SUCCESS);
@@ -63,7 +61,7 @@ int main(int argc, char * argv[]) {
   kernel = clCreateKernel(program, kernel_name, &ret);
 
   float * val = malloc (sizeof(float) * N);
-  
+
   for (int i = 0; i < N; i++) {
     val[i] = (float)i;
   }
@@ -75,11 +73,8 @@ int main(int argc, char * argv[]) {
     clEnqueueWriteBuffer(command_queue, args[i], CL_TRUE, 0, sizeof(cl_float) * N, val, 0, NULL, NULL);
     assert(ret == CL_SUCCESS);
     ret = clSetKernelArg (kernel, i, sizeof(cl_mem), (void*) &(args[i]));
-    printf ("%d\n", ret);
     assert(ret == CL_SUCCESS);
   }
-  
-  printf ("compile successful!\n");
 
   size_t local;
   ret = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
@@ -89,13 +84,12 @@ int main(int argc, char * argv[]) {
   size_t global_item_size[] = { N };
   ret = clEnqueueNDRangeKernel (command_queue, kernel, 1, NULL,
 				&global_item_size, NULL, 0, NULL, NULL);
-  printf ("NDrange %d\n", ret);
   ret = clFinish (command_queue);
   if (! (ret == CL_SUCCESS)) {
     printf ("error code: %d\n", ret);
     assert (0);
   }
-  
+
   ret = clEnqueueReadBuffer(command_queue, args[0], CL_TRUE, 0, sizeof(cl_float) * N, val, 0, NULL, NULL);
   assert (ret == CL_SUCCESS);
 
@@ -116,15 +110,11 @@ int main(int argc, char * argv[]) {
   for (int i = 0; i < arg_num; i++) {
     ret = clReleaseMemObject(args[i]);
   }
-  
+
   ret = clReleaseCommandQueue(command_queue);
   ret = clReleaseContext(context);
 
   printf("Runtime: %lfms\n", runtime);
-
   free(source_str);
-
-  printf("finished!\n");
-  fflush(stdout);
   return 0;
 }
--- mul.cl
+++ mul.cl
@@ -1,6 +1,6 @@
 kernel void hello(global float * x, global float * y) {
-
   int ind = get_global_id(0);
   float16 v = vload16 (ind, x);
   v *= 2;
   vstore16 (v, ind, x);
+}
